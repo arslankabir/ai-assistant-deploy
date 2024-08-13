deea29cd450b802d3a5aeea3d313f26eb9a2c3ef
arslankabir <34855121+arslankabir@users.noreply.github.com>
2024-08-13 18:14:09 +0400

    Update settings.py
     specify the NLTK data path in your Django settings

--- libertegpt/settings.py
+++ libertegpt/settings.py
@@ -15,6 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import nltk
+nltk.data.path.append('/var/www/liberte/ai-assistant-deploy/nltk_data')
 
 
 # Quick-start development settings - unsuitable for production
